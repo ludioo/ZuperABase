6a65ef537eaf043c458256bb1dc1249e0788696f
ludioo <panahan23@gmail.com>
2018-09-30 10:37:58 +0700

    Laut + jump error

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -31,6 +31,7 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
         textCoin.text = "Coins = " + coins.ToString();
 	}
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
     private float speed = 5f;
     private float jumpForce = 400f;
+    public int curHealth = 5;
     private bool grounded;
     private Rigidbody2D rb;
     private Animator animator;
     private SpriteRenderer ren;
 
+    public Sprite[] HeartSprites;
+    public Image HeartUI;
+    private Player player;
+
 	// Use this for initialization
 	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         ren = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -22,6 +30,7 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Debug.Log(grounded);
+        HeartUI.sprite = HeartSprites[player.curHealth];
 	}
     
     public void Jump()
@@ -56,6 +65,12 @@ public class Player : MonoBehaviour {
         if (collision.gameObject.tag == "Ground")
         {
             grounded = true;
+        }
+
+        if (collision.gameObject.tag == "Laut")
+        {
+            curHealth--;
+            SceneManager.LoadScene("Gameplay");
         }
     }
 
