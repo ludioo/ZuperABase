4bf184f47b43de1716dfb92f2ca044dcf9bc737f
Zainokta <zainokta@ymail.com>
2018-10-07 14:11:14 +0700

    add level complete

---
+++ Assets/Script/LevelFinish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelFinish : MonoBehaviour {
+
+    public Text txtCoin;
+    public Text txtHealth;
+    public Text txtTotal;
+
+	// Update is called once per frame
+	void Update () {
+        txtCoin.text = "Coins = " + GameManager.instance.coins.ToString();
+        txtHealth.text = "Health Remaining = " + Player.curHealth.ToString();
+        txtTotal.text = "Total Points = " + (GameManager.instance.coins + Player.curHealth).ToString();
+	}
+}
